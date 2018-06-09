89f63a608dbbf3da319e9ee93eea400432ae2f42
Sarah Jacob <smjacob91@gmail.com>
2018-06-09 14:08:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artist:'song titles'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist[]
+  playlist[]
 }
   describe('updatePlaylist(playlist, artistName, songTitle)', () => {
     before(() => {
