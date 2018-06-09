a8441da1e1df0f805a80b2ed872ccd7cbbc8a405
Sarah Jacob <smjacob91@gmail.com>
2018-06-09 14:09:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,17 +1,5 @@
 var playlist = {artist:'song titles'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[]
+  playlist['artistName'] = 'songTitle'
 }
-  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
-    before(() => {
-      playlist['Slowdive'] = 'Alison'
-      playlist['My Bloody Valentine'] = 'Sometimes'
-    })
-
-    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
-
-      expect(playlist).
-        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-    })
